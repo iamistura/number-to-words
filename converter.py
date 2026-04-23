4f9cce425433cec2ad52fa98bd4dc8a9039dc8f7
laykan22 <olalekanmustapha31@gmail.com>
2026-04-23 13:46:29 +0100

    Add number to words converter for numbers 1–20

---
+++ converter.py
@@ -0,0 +1,33 @@
+# number to words converter (1–20)
+
+number_words = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty"
+}
+
+# take input
+num = int(input("Enter a number (1–20): "))
+
+# check and display result
+if num in number_words:
+    print(number_words[num])
+else:
+    print("Number out of range!")
